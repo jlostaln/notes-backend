3d54af231fab56c24846a98a13627639d95a1c7f
Jarkko Stålnacke <136805206+jlostaln@users.noreply.github.com>
2024-11-08 13:22:05 +0200

    uibuild

--- index.js
+++ index.js
@@ -87,42 +87,29 @@ const generateId = () => {
   return maxId + 1
 }
 
-app.post('/api/notes', (req, res) => {
+app.post('/api/notes', (req, res, next) => {
   const body = req.body
   
-  if (body.content === undefined) {
-    return res.status(400).json({error: 'content missing'})
-  }
-
   const note = new Note({
     content: body.content,
     important: body.important || false,
   })
 
-  note.save().then(savedNote => {
+  note.save()
+    .then(savedNote => {
     res.json(savedNote)
-  })
-
-  // const note = {
-  //   content: body.content,
-  //   important: body.important || false,
-  //   id: generateId(),
-  // }
-
-  // notes = notes.concat(note)
-  // res.json(note)
-
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (req, res, next) => {
-  const body = req.body
+  const { content, important } = req.body
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
-
-  Note.findByIdAndUpdate(req.params.id, note, { new: true })
+  Note.findByIdAndUpdate(
+    req.params.id, 
+    { content, important }, 
+    { new: true, runValidators: true, context: 'query' }
+  )
     .then(updatedNote => {
       res.json(updatedNote)
     })
@@ -141,6 +128,8 @@ const errorHandler = (error, req, res, next) => {
 
   if (error.name === 'CastError') {
     return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
   }
 
   next(error)
